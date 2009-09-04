e1225f77b5317b6fc0ebdd20b52a3f689c61a881
Maxim Oleinik <maxim.oleinik@gmail.com>
2009-09-04 13:24:14 +0400

    Task: TestAll: add "--isolate" parameter to run tests is separate (parallel) processes
    PHPUnit-3.4 required

--- lib/task/sfPhpunitTestallTask.class.php
+++ lib/task/sfPhpunitTestallTask.class.php
@@ -4,37 +4,42 @@
  *
  * @package    sfPhpunitPlugin
  * @subpackage task
- * 
+ *
  * @author     Pablo Godel <pgodel@gmail.com>
  */
 class sfPhpunitTestallTask extends sfBaseTask
 {
-	/**
-	 * @see sfTask
-	 */
-	protected function configure()
-	{
-		$this->namespace = 'phpunit';
-		$this->name = 'testall';
-		$this->briefDescription = 'Runs PHPUnit AllTests';
+    /**
+     * @see sfTask
+     */
+    protected function configure()
+    {
+        $this->namespace = 'phpunit';
+        $this->name = 'testall';
+        $this->briefDescription = 'Runs PHPUnit AllTests';
 
-		$this->addOptions(array(
-		new sfCommandOption('kind', 'k', sfCommandOption::PARAMETER_OPTIONAL, 'The folder to test', ''),
-		new sfCommandOption('name', 'a', sfCommandOption::PARAMETER_REQUIRED, 'The test file name', 'AllPhpunitTests'),
-		));
+        $this->addOptions(array(
+            new sfCommandOption('kind',    'k', sfCommandOption::PARAMETER_OPTIONAL, 'The folder to test', ''),
+            new sfCommandOption('name',    'a', sfCommandOption::PARAMETER_REQUIRED, 'The test file name', 'AllPhpunitTests'),
+            new sfCommandOption('isolate', 'i',  sfCommandOption::PARAMETER_OPTIONAL, 'Run tests in separate (parallel) processes', 'off'),
+        ));
 
-		$this->detailedDescription = <<<EOF
+        $this->detailedDescription = <<<EOF
 The [phpunit:testall] task Runs PHPUnit AllTests
 EOF;
-	}
+    }
 
-	/**
-	 * @see sfTask
-	 */
-	protected function execute($arguments = array(), $options = array())
-	{
-		chdir('test/phpunit');
-		passthru('phpunit '.$options['kind'].$options['name'].'.php');
-	}
+
+    /**
+     * @see sfTask
+     */
+    protected function execute($arguments = array(), $options = array())
+    {
+        $isolate = ('on' == $options['isolate']) ? '--process-isolation' : '';
+        $file    = $options['kind'] . $options['name'] . '.php';
+
+        chdir('test/phpunit');
+        passthru("phpunit {$isolate} {$file}");
+    }
 
 }
